74422229ea5397e517e5b2dee5e4c048a9131f0a
Pgty5 <abel.quispe@ucsp.edu.pe>
2026-05-17 21:28:00 -0500

    feat: agregar constraints a la tabla estadia

---
+++ scripts_sql/02_constraints_y_indices.sql
@@ -0,0 +1,23 @@
+ALTER TABLE estadia
+ADD CONSTRAINT pk_estadia PRIMARY KEY (id_estadia);
+
+ALTER TABLE estadia
+ALTER COLUMN fch_ingreso SET NOT NULL;
+
+ALTER TABLE estadia
+ALTER COLUMN id_empleado SET NOT NULL;
+
+ALTER TABLE estadia
+ALTER COLUMN id_reserva SET NOT NULL;
+
+ALTER TABLE estadia
+ADD CONSTRAINT fk_estadia_empleado FOREIGN KEY (id_empleado)
+REFERENCES empleado(id_empleado);
+
+ALTER TABLE estadia
+ADD CONSTRAINT fk_estadia_reserva FOREIGN KEY (id_reserva)
+REFERENCES reserva(id_reserva);
+
+ALTER TABLE estadia 
+ADD CONSTRAINT ck_estadia_fechas
+CHECK (fch_salida >= fch_ingreso);
