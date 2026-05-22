7e74220a19e9d0ad1817db3c68af542c6493d990
yohandurand <yohan.durand@ucsp.edu.pe>
2026-05-22 09:33:29 -0500

    feat:Poblar tabla servicio

--- scripts_sql/03_dml_poblado_datos.sql
+++ scripts_sql/03_dml_poblado_datos.sql
@@ -1,1 +1,7 @@
 INSERT INTO
+INSERT INTO servicio (id_servicio, nombre_servicio, descripcion, precio_unitario) 
+VALUES (1, 'Hospedaje Habitacion Simple' , 'Servicio de alojamiento simple por noche', 15.00),
+(2,'Hospedaje Habitacion Doble', 'Servicio de alojamiento para dos Personas por noche', 30.00),
+(3,'Servicio de Minibar', 'Consumo de bebidas y snacks en la habitacion', 8.00),
+(4,'Acceso a Spa y Masajes','Uso de instalaciones de relajacion por hora', 20.00),
+(5,'Almuerzo Ejecutivo Hotelero','Menu completo servido en el restaurante principal',15.00);
