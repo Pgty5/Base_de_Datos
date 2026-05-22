f3ebc84ad7c73bcb84f1e3ac5be01ddae270656e
Pgty5 <abel.quispe@ucsp.edu.pe>
2026-05-22 09:34:54 -0500

    feat: poblado de datos tabla empleado

--- scripts_sql/03_dml_poblado_datos.sql
+++ scripts_sql/03_dml_poblado_datos.sql
@@ -1,4 +1,10 @@
-INSERT INTO
+INSERT INTO empleado (id_empleado, nombre, apellido, dni, telefono, correo, id_rol, id_turno) VALUES
+(1, 'Pedro', 'Salas', '40111222', '999111222', 'psalas@htel.com', 1, 1),
+(2, 'Edward', 'Poma', '40222333', '999222333', 'edwardp@htel.com', 2, 2),
+(3, 'Ronal', 'Cueto', '40333444', '999333444', 'rcuetillo@htel.com', 3, 3),
+(4, 'Alhy', 'Chura', '40444555', '999444555', 'alhyplayer15@htel.com', 4, 4),
+(5, 'Diego', 'Castro', '40555666', '999555666', 'dcastro@htel.com', 5, 5);
+
 INSERT INTO servicio (id_servicio, nombre_servicio, descripcion, precio_unitario) 
 VALUES (1, 'Hospedaje Habitacion Simple' , 'Servicio de alojamiento simple por noche', 15.00),
 (2,'Hospedaje Habitacion Doble', 'Servicio de alojamiento para dos Personas por noche', 30.00),
