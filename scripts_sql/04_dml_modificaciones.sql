0db2d1e997d8516d0e805797e5eb1b538f3a4d4d
Luis  Unancha <luis.unancha@ucsp.edu.pe>
2026-05-25 21:28:05 -0500

    data: actualizar penalidad en cancelacion_reserva

--- scripts_sql/04_dml_modificaciones.sql
+++ scripts_sql/04_dml_modificaciones.sql
@@ -1,3 +1,7 @@
 UPDATE pago
 SET estado_pago= 'Pagado'
 WHERE id_pago= 104;
+----------------------------------------------
+UPDATE cancelacion_reserva
+SET penalidad = 0.00
+WHERE id_cancelacion = 4;
